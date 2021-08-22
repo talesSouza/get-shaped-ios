8ceb4f874e7fe2267eb17b2968fdfccef20ef26b
Tales Silveira de Souza <tales-silveira@hotmail.com>
2021-08-22 00:24:10 -0300

    Updated goalsViewController

---
+++ getShapped/getShapped/Controller/GoalsViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class GoalsViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var goalSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var caloriesLabel: UILabel!
+    @IBOutlet weak var carbsQtyLabel: UILabel!
+    @IBOutlet weak var fatQtyLabel: UILabel!
+    @IBOutlet weak var proteinQtyLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
