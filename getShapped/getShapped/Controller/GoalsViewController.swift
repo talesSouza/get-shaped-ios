d47ccfd1dd6de4457860ee437e519d70c808ddd6
Tales Silveira de Souza <tales-silveira@hotmail.com>
2021-08-25 23:19:55 -0300

    Created Macro enum

--- getShapped/getShapped/Controller/GoalsViewController.swift
+++ getShapped/getShapped/Controller/GoalsViewController.swift
@@ -10,8 +10,15 @@ class GoalsViewController: UIViewController {
     @IBOutlet weak var fatQtyLabel: UILabel!
     @IBOutlet weak var proteinQtyLabel: UILabel!
     
+    // MARK: - Stored Properties
     var user: User?
     var choice: Choices?
+    var totalCalories: Double?
+    
+    // MARK: - Computed Properties
+    var labels: [UILabel] {
+        [carbsQtyLabel, fatQtyLabel, proteinQtyLabel]
+    }
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
@@ -20,16 +27,35 @@ class GoalsViewController: UIViewController {
     }
 }
 
-// MARK: - Private Methods
+// MARK: - Setup
 extension GoalsViewController {
+    
+    private func setLabel() {
+        if let calories = totalCalories {
+            caloriesLabel.text = "So you have to eat \(String(format: "%.0f", calories))"
+        }
+    }
+    
+    private func setMacroDistribution() {
+        if let totalCalories = totalCalories {
+            let carbQty = totalCalories * Macro.carb.multiplier
+            let fatQty = totalCalories * Macro.fat.multiplier
+            let proteinQty = totalCalories * Macro.protein.multiplier
+            
+            carbsQtyLabel.text = "\(String(format: "%.0f", carbQty))"
+            fatQtyLabel.text = "\(String(format: "%.0f", fatQty))"
+            proteinQtyLabel.text = "\(String(format: "%.0f", proteinQty))"
+        }
+    }
     
     private func setUI() {
         choice = Choices(rawValue: goalSegmentedControl.selectedSegmentIndex)
         if let user = user,
            let choice = choice {
-            let calories = user.tdee + choice.calc
+            totalCalories = user.tdee + choice.calc
             centerImage.image = UIImage(named: "\(choice.name)")
-            caloriesLabel.text = "So you have to eat \(String(format: "%.0f", calories))"
+            setLabel()
+            setMacroDistribution()
         }
     }
 }
--- getShapped/getShapped/Model/Macro.swift
+++ getShapped/getShapped/Model/Macro.swift
@@ -1,6 +1,44 @@
 import Foundation
 
-struct Macro {
-    let name: String
-    var qty: Int
+enum Macro: Int {
+    case carb = 0
+    case fat = 1
+    case protein = 2
+    
+    var percentage: Double {
+        switch self {
+        case .carb:
+            return 0.35
+        case .fat:
+            return 0.3
+        case .protein:
+            return 0.35
+        }
+    }
+    
+    var kcal: Double {
+        switch self {
+        case .carb:
+            return 4
+        case .fat:
+            return 9
+        case .protein:
+            return 4
+        }
+    }
+    
+    var multiplier: Double {
+        kcalFactor()
+    }
+    var macrosCalculated: Double {
+        macrosCalculated(calories: 0)
+    }
+    
+    private func kcalFactor() -> Double {
+       return percentage/kcal
+    }
+    
+    private func macrosCalculated(calories: Double) -> Double {
+        return calories * multiplier
+    }
 }
